7d20bcc81602231ca075b1e925b7b81a47ea0f35
26messias26 <messias26severino@gmail.com>
2022-10-15 23:50:25 -0300

    campos da tela de votação e tela dos candidatos

--- IFUrna/app/src/main/java/com/example/ifurna/MainActivity.kt
+++ IFUrna/app/src/main/java/com/example/ifurna/MainActivity.kt
@@ -16,20 +16,25 @@ class MainActivity : AppCompatActivity() {
 
         this.btVotar = findViewById(R.id.btVotar)
 
-//        this.btVotar.setOnClickListener{
-//            val intent = Intent(this, Votacao::class.java)
-//            resul
-//        }
-
-    }
-
-    inner class OnClick: View.OnClickListener{
-        override  fun onClick(v:View?){
-            val intent = Intent(this@MainActivity, Votacao::class.java)
-
-            if(intent.resolveActivity(packageManager) != null){
-                startActivity(intent)
-            }
+        this.btVotar.setOnClickListener{
+            telaVotar()
         }
+
+
+
     }
+    private fun telaVotar(){
+        val intent = Intent(this,Votacao::class.java)
+        startActivity(intent)
+    }
+
+//    inner class OnClick: View.OnClickListener{
+//        override  fun onClick(v:View?){
+//            val intent = Intent(this@MainActivity, Votacao::class.java)
+//
+//            if(intent.resolveActivity(packageManager) != null){
+//                startActivity(intent)
+//            }
+//        }
+//    }
 }
--- IFUrna/app/src/main/java/com/example/ifurna/Votacao.kt
+++ IFUrna/app/src/main/java/com/example/ifurna/Votacao.kt
@@ -2,10 +2,70 @@ package com.example.ifurna
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextUtils
+import android.text.TextWatcher
+import android.widget.EditText
+import android.widget.TextSwitcher
 
 class Votacao : AppCompatActivity() {
+
+    private lateinit var etINum : EditText
+    private lateinit var etIINum : EditText
+    private lateinit var etIIINum : EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_votacao)
+
+        this.etINum = findViewById(R.id.etINum)
+        this.etIINum = findViewById(R.id.etIINum)
+        this.etIIINum = findViewById(R.id.etIIINum)
+
+        //TROCA O FOCO AO DIGITAR O NUMERO
+        etINum.addTextChangedListener( object : TextWatcher{
+
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+
+            override fun afterTextChanged(p0: Editable?) {
+                if (!TextUtils.isEmpty(etINum.getText())){
+                    etIINum.requestFocus()
+                }
+            }
+        });
+
+        etIINum.addTextChangedListener( object : TextWatcher{
+
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+
+            override fun afterTextChanged(p0: Editable?) {
+                if (TextUtils.isEmpty(etIINum.getText())){
+                    etINum.requestFocus()
+                }else{
+                    etIIINum.requestFocus()
+                }
+            }
+        });
+
+        etIIINum.addTextChangedListener( object : TextWatcher{
+
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+
+            override fun afterTextChanged(p0: Editable?) {
+                if (TextUtils.isEmpty(etIIINum.getText())){
+                    etIINum.requestFocus()
+
+                }
+            }
+        });
+
+
     }
 }
